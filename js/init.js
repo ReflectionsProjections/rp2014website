4ec12f9e29f305b3e1c9502e99ed4d43cf5b1402
bjryan2 <bjryan2@illinois.edu>
2014-03-29 21:12:29 -0500

    main pages and basic framework

--- js/init.js
+++ js/init.js
@@ -1,15 +1,9 @@
-/*
- * Made by WebDesignCrowd
- * http://webdesigncrowd.com
- *
- */
- 
 (function($){
 	$(function(){
-	  
+
     var navbarHeight = 80;
 
-    // Slide in Functionality 
+    // Slide in Functionality
     $(window).scroll(function() {
       var top = $(window).scrollTop();
       $(".slide-in").each(function () {
@@ -18,7 +12,7 @@
         if ((top > (thisTop - (height * 1.5))) && !$(this).hasClass("slid")) {
           $(this).addClass("slid");
         }
-      });   
+      });
       $('body').scrollspy({ offset: navbarHeight+10, target: '#navbar' });
     });
 
@@ -35,8 +29,8 @@
           return (this.top = $(window).height() - navbarHeight)
         }
       }
-    })
-    
+    });
+
 
     // Parallax Scripts
     function updateParallax() {
@@ -48,20 +42,20 @@
           var scrollTop = $(window).scrollTop() + windowHeight;
           var fromTop = 0;
           var isHome = true;
-          if (top === 0) { 
-            fromTop = $(window).scrollTop() - top; 
+          if (top === 0) {
+            fromTop = $(window).scrollTop() - top;
             isHome = true;
           }
-          else { 
-            fromTop = $(window).scrollTop() - top + windowHeight; 
+          else {
+            fromTop = $(window).scrollTop() - top + windowHeight;
             isHome = false;
           }
-          if ((bottom > $(window).scrollTop()) && (top < scrollTop)) {   
+          if ((bottom > $(window).scrollTop()) && (top < scrollTop)) {
             var parallax = -1 * (fromTop / 3);
             var revParallax = parallax;
             var percent = 1 - 1.3 *($(window).scrollTop() / $(window).height());
-            if (isHome) { 
-              revParallax += navbarHeight; 
+            if (isHome) {
+              revParallax += navbarHeight;
               $("#home .logo").css('marginTop', parallax + "px");
               $("#home .logo, #home .welcome, #home .call-to-action, #home .macbook-preview").css('opacity', percent);
             }
@@ -71,10 +65,10 @@
       }
     }
     updateParallax();
-    
+
     $(window).scroll(function() {
       updateParallax();
-      // if ($(window).height() > $(window).scrollTop()) {   
+      // if ($(window).height() > $(window).scrollTop()) {
       //   var parallax = -1 * ($(window).scrollTop() / 3);
       //   var revParallax = navbarHeight + parallax;
       //   var percent = 1 - 1.5 *($(window).scrollTop() / $(window).height());
@@ -83,57 +77,34 @@
       //   $("#home .logo, #home .welcome, #home .call-to-action").css('opacity', percent);
       // }
     });
-    
-      
-    // Contact Form Icon
-    $("form .form-control").focus(function() {
-      $(this).siblings("label").first().children("i").first().css({"color": "#aaa", "left": 0});
-    });
-    $("form .form-control").blur(function() {
-      $(this).siblings("label").first().children("i").first().css({"color": "transparent", "left": "-20px"});
-    });
-	  
-    // Blog Masonry
-    var $container = $('.masonry-grid');
-    
-    $container.imagesLoaded(function(){
-      new AnimOnScroll( document.getElementById( 'grid' ), {
-        minDuration : 0.4,
-        maxDuration : 0.7,
-        viewportFactor : 0.2
-      } );
 
-      // Smooth Scrolling
-      $("a.scroll").click(function(e) {
-        e.preventDefault();
-        var offset = $(this.hash).offset().top - (navbarHeight/2);
-        $('html, body').animate({ scrollTop: offset }, 600);
-      });
-    });
+
+
 
 
     // CONTACT PAGE
 
       function initialize() {
-        var myLatLong = new google.maps.LatLng(40.7556,-73.9797);
+        var myLatLong = new google.maps.LatLng(40.114312,-88.22484);
         var mapOptions = {
           center: myLatLong,
           scrollwheel: false,
-          zoom: 13
+          zoom: 15,
         };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
+
+        var map = new google.maps.Map(document.getElementById("mapwrap"),
             mapOptions);
-        // To add the marker to the map, use the 'map' property
+
         var marker = new google.maps.Marker({
             position: myLatLong,
             map: map,
-            title:"123 Broadway"
+            title:"Thomas M. Siebel Center for Computer Science"
         });
 
       }
       google.maps.event.addDomListener(window, 'load', initialize);
-    
-    
+
+
 
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
