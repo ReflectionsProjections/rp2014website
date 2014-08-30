a1a8b3c541705bf13f519e3975f5ef2f36dff938
bjryan2 <bjryan2@illinois.edu>
2014-08-30 13:25:49 -0500

    new speakers

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ var gulp = require('gulp')
  * PATH CONSTANTS
  */
 
-var BASE_PATH = "./";
+var BASE_PATH = "_site/";
 var DIST_PATH = BASE_PATH + "dist/";
 var IMG_PATH = BASE_PATH + "img/**/*";
 var COMPANY_IMG_PATH = BASE_PATH + "img/**/*";
